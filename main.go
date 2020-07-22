0c81c08d91f3278cd28acbf0a48de6fa462b9528
kiyocy24 <kiyocy24@gmail.com>
2020-07-23 00:10:28 +0900

    to merge commit

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 	"cloud.google.com/go/pubsub"
 	"github.com/bwmarrin/discordgo"
 	"github.com/pkg/errors"
-	"google.golang.org/api/option"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -125,13 +124,14 @@ func pullMsgsSync(w io.Writer, subID string) error {
 	}
 
 	ctx := context.Background()
-	client, err := pubsub.NewClient(ctx, projectID, option.WithAPIKey(GCPAPIkeyEnv))
+	client, err := pubsub.NewClient(ctx, projectID)
 	if err != nil {
 		return fmt.Errorf("%v", err)
 	}
 	defer client.Close()
 
 	sub := client.Subscription(subID)
+
 	exists, err := sub.Exists(ctx)
 	if err != nil {
 		return fmt.Errorf("%v", err)
